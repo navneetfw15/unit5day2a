3c34a23f749135baee149bb5986bcbda6bdfe9e3
navneetfw15 <navneet638864@gmail.com>
2022-04-10 23:34:34 +0530

    a

--- assimeant/src/App.js
+++ assimeant/src/App.js
@@ -1,38 +1,38 @@
-// import './App.css';
+import './App.css';
 
-// function App() {
+function App() {
   
-// const card = [
-//   {
-//     heading:"Mobile Operating System",
-//     list:["Andriod","Blackberry","IPhone","Windows Phone"],
-//   },{
-//     heading:"Mobile Mnufacturers",
-//     list:["Samsung","HTC","Micromax","Aplle"],
-//   },
-// ]
+const card = [
+  {
+    heading:"Mobile Operating System",
+    list:["Andriod","Blackberry","IPhone","Windows Phone"],
+  },{
+    heading:"Mobile Mnufacturers",
+    list:["Samsung","HTC","Micromax","Aplle"],
+  },
+]
 
-//   return (
-//     <div>
-//       {card.map((ele)=>{
-//         return <Cards ele={ele}></Cards> 
-//       })}
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      {card.map((ele)=>{
+        return <Cards ele={ele}></Cards> 
+      })}
+    </div>
+  );
+}
 
-// function Cards({ele}){
-//   return <div>
-//     <h1>{ele.heading}</h1>
-//     <ul>
-//       {ele.list.map((el)=>{
-//        return  <li>{el}</li>
-//       })}
-//     </ul>
-//   </div>;
-//  }
+function Cards({ele}){
+  return <div>
+    <h1>{ele.heading}</h1>
+    <ul>
+      {ele.list.map((el)=>{
+       return  <li>{el}</li>
+      })}
+    </ul>
+  </div>;
+ }
 
-// export default App;
+export default App;
 
 
 
