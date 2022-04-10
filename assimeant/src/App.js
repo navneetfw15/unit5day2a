cce17e39fdc613d1c8aac9e723632cb26ab8df5d
navneetfw15 <navneet638864@gmail.com>
2022-04-10 21:38:43 +0530

    test file

---
+++ assimeant/src/App.js
@@ -0,0 +1,43 @@
+// import './App.css';
+
+// function App() {
+  
+// const card = [
+//   {
+//     heading:"Mobile Operating System",
+//     list:["Andriod","Blackberry","IPhone","Windows Phone"],
+//   },{
+//     heading:"Mobile Mnufacturers",
+//     list:["Samsung","HTC","Micromax","Aplle"],
+//   },
+// ]
+
+//   return (
+//     <div>
+//       {card.map((ele)=>{
+//         return <Cards ele={ele}></Cards> 
+//       })}
+//     </div>
+//   );
+// }
+
+// function Cards({ele}){
+//   return <div>
+//     <h1>{ele.heading}</h1>
+//     <ul>
+//       {ele.list.map((el)=>{
+//        return  <li>{el}</li>
+//       })}
+//     </ul>
+//   </div>;
+//  }
+
+// export default App;
+
+
+
+
+
+
+
+
